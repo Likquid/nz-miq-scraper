1aba82df4728934bd42f7a7dae22c9a5dd875123
Billy Li <bl@medipass.io>
2021-08-24 16:50:47 +1000

    fix

--- src/util/sendSlackMessage.js
+++ src/util/sendSlackMessage.js
@@ -2,5 +2,5 @@ const got = require('got');
 
 exports.delayedResponse = async (responseUrl, data) => {
     console.log("sending message to slack");
-    await got.post(responseUrl, { body: data });
+    await got.post(responseUrl, { json: data });
 };
